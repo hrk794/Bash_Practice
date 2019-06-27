4d02f30af0cf44d79e334e388c3b779fccf58b11
hrk794 <hrk794@gmail.com>
2019-06-27 11:48:25 +0530

    Exercise 1 in Chapter 1 of Pro Bash Programming

---
+++ ex1.1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Write a script that creates a directory called bpl inside $HOME. Populate this
+# directory with two subdirectories, bin and scripts.
+
+cd $HOME
+mkdir bpl
+cd bpl
+mkdir bin scripts
+
+exit
+
